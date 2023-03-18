8cf6b0f9376c4b5c3a07aee78cc3baca47ffec4c
Leonid Shvab <matembit1996@gmail.com>
2023-03-18 21:06:05 +0200

    infra: setup svgLoader

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from "vite-svg-loader"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    svgLoader({
+      svgoConfig: {
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                removeDimensions: false,
+                removeViewBox: false,
+                inlineStyles: { onlyMatchedOnce: false },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
